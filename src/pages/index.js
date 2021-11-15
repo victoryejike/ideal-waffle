f9f5e132b4d39d219e8d53827a8a629fca329483
Ejike Victory <victoryejike@gmail.com>
2021-11-15 14:25:16 +0100

    feat: starter pack for gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h2>Design</h2>
+      <h3>Design and Develop</h3>
+      <p>UX designer & web developer based in Nigeria.</p>
+    </div>
+  )
 }
