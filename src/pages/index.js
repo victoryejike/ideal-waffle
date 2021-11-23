9b764bb6550bd7035d37e78c4f5117689bb73d36
Ejike Victory <victoryejike@gmail.com>
2021-11-23 17:35:15 +0100

    component styling using css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
+import { Link } from "gatsby";
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from '../styles/home.module.css';
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Design and Develop</h3>
           <p>UX designer & web developer based in Nigeria.</p>
+          <Link to='/projects' className={styles.btn}>My Portfolio projects</Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import * as styles from '../../styles/projects.module.css';
 
 export default function Projects () {
   return (
     <Layout>
-      <div>
-        <p>Projects</p>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects and Websites I have created</h3>
       </div>
     </Layout>
   )
