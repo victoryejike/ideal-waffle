2b95f6d4928bb19bbc059ebb6db6c7049cac4781
Ejike Victory <victoryejike@gmail.com>
2021-11-23 18:04:36 +0100

    making queries with graphql in pages using sitemetadata as example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css';
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data);
+  const { title, description } = data.site.siteMetadata
   return (
     <Layout>
       <section className={styles.header}>
@@ -13,7 +15,21 @@ export default function Home() {
           <p>UX designer & web developer based in Nigeria.</p>
           <Link to='/projects' className={styles.btn}>My Portfolio projects</Link>
         </div>
+        <img src='/banner.png' alt='site-banner' style={{ maxWidth: '100%' }}/>
+        <p>{ title } - { description }</p>
       </section>
     </Layout>
   )
 }
+
+export const query = graphql
+`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
