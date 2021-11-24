229a14d50eda436fa43603c3cd8e8639be76a10b
Ejike Victory <victoryejike@gmail.com>
2021-11-24 08:35:19 +0100

    sorting of queries

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -31,7 +31,7 @@ export default function Projects ({ data }) {
 export const query = graphql
 `
   query MyQuery {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
         frontmatter {
           slug
