3e8dc3fdec4a42ef02d87bb32ed6311203cb00ee
Ejike Victory <victoryejike@gmail.com>
2021-11-24 14:19:38 +0100

    feature images using gatsbyimages

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,11 +2,13 @@ import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Layout from '../../components/Layout';
 import * as styles from '../../styles/projects.module.css';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export default function Projects ({ data }) {
   console.log(data);
   const projects = data.projects.nodes
   const contact = data.contact.siteMetadata.contact
+  // const images = getImage(data.projects.nodes.frontmatter.thumb.childImageSharp.gatsbyImageData.images.fallback.src)
 
   return (
     <Layout>
@@ -18,6 +20,7 @@ export default function Projects ({ data }) {
             projects.map(project => (
               <Link to={ `/projects/${project.frontmatter.slug}` } key={project.id} >
                 <div>
+                  <GatsbyImage image={getImage(project.frontmatter.thumb.childImageSharp.gatsbyImageData)} alt='Banner' />
                   <h3>{ project.frontmatter.title }</h3>
                   <p>{ project.frontmatter.stack }</p>
                 </div>
@@ -40,6 +43,15 @@ export const query = graphql
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              gatsbyImageData(
+                width: 200
+                placeholder: BLURRED
+                formats: [AUTO, WEBP, AVIF]
+              )
+            }
+          }
         }
         id
       }
