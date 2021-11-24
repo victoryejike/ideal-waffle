930171ae7a4e6f81fb2342d4409274e8eaa3c554
Ejike Victory <victoryejike@gmail.com>
2021-11-24 10:41:55 +0100

    using gatsby to optimise images

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby";
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css';
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Home(props) {
   return (
@@ -13,7 +14,7 @@ export default function Home(props) {
           <p>UX designer & web developer based in Nigeria.</p>
           <Link to='/projects' className={styles.btn}>My Portfolio projects</Link>
         </div>
-        <img src='/banner.png' alt='site-banner' style={{ maxWidth: '100%' }}/>
+        <StaticImage src="../images/banner.png" alt="Banner" placeholder="blurred" />
       </section>
     </Layout>
   )
